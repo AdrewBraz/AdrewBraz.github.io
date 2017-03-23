43b0d117a7dedb9e3c2a62e62cf3673546944417
AndrewBraz <andbrazhnikov@gmail.com>
2017-03-23 16:13:24 +0300

    new commit

---
+++ Sedona/js/script.js
@@ -0,0 +1,67 @@
+(function(){
+  var form = document.querySelector('.search-content__form');
+  var search = document.querySelector('.search-content__btn');
+  var adult = document.getElementById('adult-field');
+  var child = document.getElementById('child-field');
+  var nav = document.querySelector('.main-nav')
+  var adultElement = document.querySelector('.search-form__adult-quantity');
+  var childElement = document.querySelector('.search-form__child-quantity');
+  var toggle = document.getElementById('toggle');
+  var close = document.getElementById('close');
+  var header = document.querySelector('.page-header__logo-wrap')
+  var highLimit = 10;
+  var lowLimit = 0;
+  
+  toggle.onclick = function(event){
+    event.preventDefault();
+    toggle.classList.toggle('page-header__toggle--close');
+    header.classList.toggle('page-header__logo-wrap--hidden');
+    nav.classList.toggle('main-nav--visible');
+    close.classList.toggle('close-btn--visible');
+  }
+    
+  search.onclick = function(event){
+    form.classList.add('search-content__form-open');
+  }
+  
+  function Counter(elem){
+    var currentcounter = 0;
+    var self = this;
+    
+    this.plus = function(){
+      currentcounter++;
+      if(currentcounter > 10){
+        currentcounter = 10;
+        return currentcounter;
+      }
+      return currentcounter;
+    }
+    
+    this.minus = function(){
+      currentcounter--;
+      if(currentcounter < 0){
+        currentcounter = 0;
+        return currentcounter;
+      }
+      return currentcounter;
+    }
+        
+    
+    elem.onclick = function(e){
+      e.preventDefault();
+      var target = e.target;
+      var action = target.getAttribute('data-counter');
+      if(action == 'minus'){
+        target.nextElementSibling.value = self[action]();
+      }
+      
+      if(action == 'plus'){
+        target.previousElementSibling.value = self[action]();
+      }
+    }
+  }
+  
+  new Counter(adultElement);
+  new Counter(childElement);
+  
+})()
